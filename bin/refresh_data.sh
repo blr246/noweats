e5ab2128ff43b6c3908d04ae5bda30e02273a9c6
Brandon Reiss <brandon.l.reiss@gmail.com>
2014-04-26 16:55:50 -0400

    Script to fix bz2 data for Python.
    After re-compressing data with bzip2, Python seems happy.

---
+++ bin/refresh_data.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# Overcome the issues with Python's bz2 by decompressing and the compresssing
+# the data.
+
+#set -x
+
+usage() {
+  script_name=`basename $0`
+  cat <<EOF
+Usage: ${script_name} DATA_DIR
+
+  Uncompress and then compress all files in DATA_DIR using bzip2.
+
+  DATA_DIR    data file directory
+EOF
+}
+
+# Check arguments.
+if [ $# -ne 1 ]; then
+  echo Invalid arguments 1>&2
+  usage
+  exit 1
+fi
+
+data_dir=$1; shift
+
+# Get list of files.
+for file in `find "${data_dir}" -type f`; do
+  if echo "${file}" | grep -Eq '(\.bz2$|\.tmp$)'; then
+    continue
+  fi
+  echo "Re-compressing ${file}"
+  tmp_file="${file}.tmp"
+  bzip2 -cd "${file}" >"${tmp_file}"
+  bzip2 -z "${tmp_file}"
+  mv "${tmp_file}.bz2" "${file}"
+done
+
+exit 0
