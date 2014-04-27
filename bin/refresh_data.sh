7615a225fa2f3077f316244f6df98c07958285c5
Brandon Reiss <blr246@nyu.edu>
2014-04-26 23:14:19 -0400

    Abolute paths for use with cron.
    The re-compress script now uses absolute paths so that it can run in the
    cron environment.

--- bin/refresh_data.sh
+++ bin/refresh_data.sh
@@ -4,9 +4,20 @@
 
 #set -x
 
+# Collect absolute paths to binaries.
+
+mv='/bin/mv'
+find='/usr/bin/find'
+cat='/bin/cat'
+basename='/bin/basename'
+echo='/bin/echo'
+
+bzip2='/usr/bin/bzip2'
+grep='/bin/grep'
+
 usage() {
-  script_name=`basename $0`
-  cat <<EOF
+  script_name=`${basename} $0`
+  ${cat} <<EOF
 Usage: ${script_name} DATA_DIR
 
   Uncompress and then compress all files in DATA_DIR using bzip2.
@@ -17,23 +28,23 @@ EOF
 
 # Check arguments.
 if [ $# -ne 1 ]; then
-  echo Invalid arguments 1>&2
+  ${echo} Invalid arguments 1>&2
   usage
   exit 1
 fi
 
 data_dir=$1; shift
 
-# Get list of files.
-for file in `find "${data_dir}" -type f`; do
-  if echo "${file}" | grep -Eq '(\.bz2$|\.tmp$)'; then
+# Get list of files that have been rollwed over (have a dot in them).
+for file in `${find} "${data_dir}" -type f -name '*.*'`; do
+  if ${echo} "${file}" | ${grep} -Eq '(\.bz2$|\.tmp$)'; then
     continue
   fi
-  echo "Re-compressing ${file}"
+  ${echo} "Re-compressing ${file}"
   tmp_file="${file}.tmp"
-  bzip2 -cd "${file}" >"${tmp_file}"
-  bzip2 -z "${tmp_file}"
-  mv "${tmp_file}.bz2" "${file}"
+  ${bzip2} -cd "${file}" >"${tmp_file}"
+  ${bzip2} -z "${tmp_file}"
+  ${mv} "${tmp_file}.bz2" "${file}"
 done
 
 exit 0
