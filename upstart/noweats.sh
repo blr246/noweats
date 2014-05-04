fbf2cb7ecd471839c611264b9bb9fd87d16300f9
Brandon L. Reiss <brandon.l.reiss@gmail.com>
2014-05-04 13:28:55 -0400

    Log collect_nyc to syslogd.
    In the upstart job, we would like to know about disconnects from Twitter
    and any other relevant events.

--- upstart/noweats.sh
+++ upstart/noweats.sh
@@ -12,7 +12,7 @@ trap "cleanup" SIGINT SIGTERM EXIT
 /bin/mkdir -p /home/reissb/noweats_data/collect
 . /home/reissb/.virtualenvs/noweats/bin/activate
 # Collect tweets in the background.
-collect_nyc /home/reissb/noweats_data/collect &
+collect_nyc /home/reissb/noweats_data/collect 2>&1 | /usr/bin/logger -t noweats &
 # Process tweets every 5 minutes. This helps when we fall behind.
 while true; do
   /usr/bin/nice -n 19 process_new \
